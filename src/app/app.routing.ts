83e40655cd0290b19b952a3ea57f8ed1e4425316
NicolasBouhours <nico53960@hotmail.fr>
2016-11-21 23:55:36 +0100

    STEP 86  - Redirecting Request

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,8 +6,10 @@ import { USER_ROUTES } from './user/user.routes';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'user/', redirectTo: '/user/1', pathMatch: 'full' },
     { path: 'user/:id', component: UserComponent },
-    { path: 'user/:id', component: UserComponent, children: USER_ROUTES }
+    { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
+    {path: '**', redirectTo: '/user/1'}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
