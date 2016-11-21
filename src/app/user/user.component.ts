b182ace221c0dd70b3b24b681d20d569882fd2d4
NicolasBouhours <nico53960@hotmail.fr>
2016-11-21 22:00:12 +0100

    Understanding Navigation Paths

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,8 @@ import { Component, OnDestroy } from '@angular/core';
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <a [routerLink]="['../']">Home</a>
+      <a [routerLink]="['/user']">User</a>
     `
 })
 export class UserComponent {
